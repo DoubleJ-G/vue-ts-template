f1c4e9ecddb4f4d674dabaa45399ae848f3da089
Jaden Gregory <dev@jadengregory.com.au>
2021-12-04 18:34:05 +1100

    Update project information

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <ProjectInfo msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
+
+<script setup lang="ts">
+import ProjectInfo from './components/ProjectInfo.vue';
+</script>
 
 <style>
 #app {
@@ -16,6 +14,13 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
