a4d659983b209a0b58ab8401ed0c0c3fbf55ba8a
Jaden Gregory <dev@jadengregory.com.au>
2021-12-04 18:45:12 +1100

    Linting fixes

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,23 @@
 </template>
 
 <script setup lang="ts">
-import ProjectInfo from './components/ProjectInfo.vue';
+  import ProjectInfo from './components/ProjectInfo.vue'
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 
-  width: 100vw;
-  height: 100vh;
+    width: 100vw;
+    height: 100vh;
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
