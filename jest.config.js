755f1918258cb30caeafe0ec594b77233397b9f7
Jaden Gregory <dev@jadengregory.com.au>
2021-12-04 19:58:25 +1100

    Fixes jest failing tests with asset imports

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$':
+      'jest-transform-stub',
   },
 }
